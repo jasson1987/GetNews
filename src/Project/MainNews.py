0af4ca5ab409f9beae92cedd1ea64c51f6b92295
jasson.xu <jasson.xu@qq.com>
2018-05-07 11:10:44 +0800

    "note cancel"

--- src/Project/MainNews.py
+++ src/Project/MainNews.py
@@ -19,31 +19,31 @@ from Func import ImportExcel
                     11:'浙江',12:'安徽',13:'福建',14:'江西',15:'河南',16:'深圳',17:'广东',18:'广西',19:'海南',20:'重庆',
                     21:'四川',22:'云南',23:'甘肃',24:'青海',25:'宁夏',26:'湖北',27:'湖南',28:'新疆',29:'贵州'}
 '''
-# #各省份新闻
-# lotpro = GetProvNews()
-# try:
-#     lotpro.GetNewProv([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
-#     print 'Import Province News Successfully'
-# except:
-#     print 'Import Province News Fail'
+#各省份新闻
+lotpro = GetProvNews()
+try:
+    lotpro.GetNewProv([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
+    print 'Import Province News Successfully'
+except:
+    print 'Import Province News Fail'
        
     
-# #门户网站新闻
-# lot = GetWebNews()
-# try:
-#     lot.GetNewWeb(['中彩','新浪','网易','中国福彩','凤凰网','彩客网','新华网','搜狐网','500彩票网'], -1, 20)
-#     print 'Import Web News Successfully'
-# except:
-#     print 'Import Web News Fail'
-#        
-# #输出Excel
-# ImportExcel()
+#门户网站新闻
+lot = GetWebNews()
+try:
+    lot.GetNewWeb(['中彩','新浪','网易','中国福彩','凤凰网','彩客网','新华网','搜狐网','500彩票网'], -1, 20)
+    print 'Import Web News Successfully'
+except:
+    print 'Import Web News Fail'
+        
+#输出Excel
+ImportExcel()
 
 
 # lotpro = GetProvNews()
 # lotpro.GetNewProv([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
 
 
-lot = GetWebNews()
+# lot = GetWebNews()
 # lot.GetNewWeb(['中彩','新浪','网易','中国福彩','凤凰网','彩客网','新华网','搜狐网','500彩票网'], -1, 20)
-lot.GetNewWeb(['中彩'], -1, 1)
+# lot.GetNewWeb(['中彩'], -1, 1)
